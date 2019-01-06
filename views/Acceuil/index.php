ae9eb2f92b5ef4798a299bfc6e034c7d1af943d6
Nathan <nathan.krulic@etu.uca.fr>
2019-01-06 12:14:22 +0100

    petites modifs front end

--- views/Acceuil/index.php
+++ views/Acceuil/index.php
@@ -1,22 +1,10 @@
 
-<!---------------- Content ------------------->
-
 
 <?php foreach ($articles as $a): ?>
-    <br>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-                <div class="post-preview">
-                    <a href="<?php echo WEBROOT;?>Acceuil/views/<?php echo $a['id_article']; ?>">
-                        <h2 class="post-title"><?php echo $a['titre']; ?></h2>
-                    </a>
-                    <p class="post-meta"><?php echo $a['date_publication'] ?></p>
-                </div>
-
-            </div>
-        </div>
-    </div>
+    <a href="<?php echo WEBROOT;?>Acceuil/views/<?php echo $a['id_article']; ?>">
+        <h2 class="post-title"><?php echo $a['titre']; ?></h2>
+    </a>
+    <p class="post-meta"><?php echo $a['date_publication'] ?></p>
     <hr>
 <?php endforeach ?>
 
--- views/layout/default.php
+++ views/layout/default.php
@@ -67,7 +67,17 @@
 <!---------------- Content here  ------------------->
 
 
-<?php echo $content_layout; ?>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-preview">
+             <?php echo $content_layout; ?>
+            </div>
+
+        </div>
+    </div>
+</div>
 
 
 
